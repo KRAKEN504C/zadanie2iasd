6894720bd139be07d3276c53eddb6af85e57214a
KRAKEN504C <32463878+KRAKEN504C@users.noreply.github.com>
2017-11-07 23:43:55 +0100

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,73 +1,162 @@
 #include <iostream>
-#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
 #include <fstream>
 
 using namespace std;
 int xlab, ylab;
-char **lab;
 
-void Lucas(int xlab, int ylab);
+void Lucas();
 
+void Printing(int xlab,int ylab,char** lab);
+
+void botStart(int xlab, int ylab, char** lab, int *startPointx, int *startPointy);
+void botMove(int xlab, int ylab, char** lab, int *startPointx, int *startPointy);
 
 int main()
 {
+	int checkExit = 0;
+	int StrPtx, StrPty;
+	int  *startPointx = &StrPtx;
+	int  *startPointy = &StrPty;
+	//char **lab;
 	fstream plik;
 	plik.open("labirynt.txt", ios::in | ios::out);
 	if (plik.good() == true)
 	{
+
+		plik >> xlab;
+		plik >> ylab;
+
+		int fragmentLabiryntu;
+
+		char** lab = new  char*[xlab];
+		for (int i = 0; i < xlab; ++i)
+			lab[i] = new char[ylab];
+
 		{
-			plik >> xlab;
-			plik >> ylab;
 
-			int litera = 33;
-
-			lab = (char**)malloc(xlab * sizeof(char *)); //alokacja pamieci
 			for (int i = 0; i < xlab; i++)
 			{
-				lab[i] = (char*)malloc(ylab * sizeof(char));
-
 				for (int j = 0; j < ylab; ++j) //wpisanie wartosci do tablicy
 				{
-					lab[i][j] = (litera);
-					litera++;
+					plik >> fragmentLabiryntu;
+					switch (fragmentLabiryntu)
+					{
+					case 0:
+						lab[i][j] = 32;
+						break;
+					case 1:
+						lab[i][j] = 219;
+						break;
+					case 2:
+						lab[i][j] = 176;
+						break;
+					case 3:
+						lab[i][j] = 178;
+						break;
+					}
 				}
+			}
 
-			}
 			cout << "xlab== " << xlab << endl;
 			cout << "ylab== " << ylab << endl;
 
-			for (int i = 0; i < xlab; ++i, printf("\n"))
-				for (int j = 0; j < ylab; ++j)
-					printf("%.1c\t", lab[i][j]);
+			botStart(xlab, ylab, lab, startPointx, startPointy);
+			Printing(xlab, ylab,lab);
+			do
+			{
+				botMove(xlab, ylab, lab);
+
+			} while (checkExit != 0);
+			
+			
 		}
 
 
 
-
 		plik.close();
+	}else //gdyby z jakiegoœ powodu plik zosta³ nieprawidlowo odczytany, lub nie odczytany w ogóle. (przypomne, ze plik ma sie nazywac labirynt.txt) 
+	{
+		cout << "Error! Blad odczytu pliku!" << endl;
+		system("Pause");
+		return 0;
 	}
-
 	system("Pause");
 
+
+	//Lucas();
 	return 0;
 }
 
-/*
-void Lucas(int xlab, int ylab)
+
+void Printing(int xlab, int ylab,char** lab)
 {
-	int l=0,n=0;
-
-	if (n == 0)
-	{
-		l = 2;
-	}else
-	{if(n==1) }
-
-	for (;n > 1;)
-	{
-
-	}
+	for (int i = 0; i < xlab; ++i, printf("\n"))
+		for (int j = 0; j < ylab; ++j)
+			cout << (char)lab[i][j];
 }
-*/
 
-//char lab[xlab][ylab] = {};
+void botStart(int xlab, int ylab, char** lab, int *startPointx, int *startPointy)
+{
+	int startx = -1, starty = -1;
+	for (int i = 0; i < xlab; i++)
+	{
+		for (int j = 0; j < ylab; j++)
+		{
+			//cout << "lab[i][j]== " << (int)lab[i][j] << endl;
+			if (lab[i][j] == -80)
+			{
+				startx = i;
+				starty = j;
+			}
+		}
+	}
+	if (startx == -1 || starty == -1)
+	{
+		cout << "Error! Nie zanaleziono punktu Startu!" << endl;
+	}
+	else
+	{
+		lab[startx][starty] = 248;
+	}
+	*startPointx = startx;
+	*startPointy = starty;
+	//cout << "startPointx== " << *startPointx << endl;
+	//cout << "startPointy== " << *startPointy << endl;
+}
+
+void botMove(int xlab, int ylab, char** lab, int *startPointx, int *startPointy)
+{
+
+}
+
+
+void Lucas()
+{
+	int n0=2,n1=1,input=0;
+	long long int n;
+
+	cout << "wprowadz numer iczby lukasa, jaka chcesz otrzymac: ";
+	cin >> input;
+	cout << "twoja liczba to: " << input << endl;
+
+	if (input == 0)
+	{
+		cout << "zerowa liczba Lucasa wynosi " << n0 << endl;
+	}else if (input == 1) {
+		cout << "pierwsza liczba lucasa wynosi " << n1 << endl;
+	}
+	else
+		cout << input;
+	do
+	{
+		n = n1 + n0;
+		n0 = n1;
+		n1 = n;
+		input--;
+	} while (input > 1);
+	cout << " liczba Lucasa wynosi: " << n;
+	system("Pause");
+}
+
