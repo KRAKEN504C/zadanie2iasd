f620ff82fa8fca8ad16358f995dd9d4c50502847
KRAKEN504C <32463878+KRAKEN504C@users.noreply.github.com>
2017-11-15 10:15:04 +0100

    Add files via upload

--- Main - Kopia (2).cpp
+++ Main - Kopia (2).cpp
@@ -1,36 +1,16 @@
-ï»¿#include <iostream>
+#include <iostream>
 #include <cstdio>
 #include <cstdlib>
 #include <fstream>
 #include <Windows.h>
 
-/*
-UWAGA!!!
-X sa zamienione z Y!!!
-x-to wysokosc a y-to szerokosc
-
-kiedys moze to naprawie.
-(powstrzymuje smiech)
-
-w ramach przypomnienia:
-
-BOT = 248
-SPACJA = 32
-SCIANA = -37 (albo 219, zalezy co sprawdza)
-P.STARTU / PRZEBYTA DROGA = 176
-P.WYJSCIA = 178 (-78)
-ROZGALEZIENIE = 254
-*/
-
-
-
 using namespace std;
 int xlab, ylab;
 
-int testExitx=-1;
-int testExity=-1;
+int testExitx = -1;
+int testExity = -1;
 int ilRuchow = 0;
-int crosswayLastMove=-1;
+int crosswayLastMove = -1;
 bool wDrugaStrone = false;
 
 int Lucas(int n);
@@ -70,7 +50,7 @@ int main()
 	bool *ptravaSouth = &avaSouth;
 	bool *ptravaWest = &avaWest;
 
-	//punkt rozdroÅ¼a.
+	//punkt rozdro¿a.
 	int crosswayPointx = -1, crosswayPointy = -1;
 	int *ptrcrosswayPointx = &crosswayPointx;
 	int *ptrcrosswayPointy = &crosswayPointy;
@@ -85,8 +65,6 @@ int main()
 		plik >> ylab;
 
 		int fragmentLabiryntu;
-
-
 
 		char** lab = new  char*[xlab];
 		for (int i = 0; i < xlab; ++i)
@@ -117,26 +95,19 @@ int main()
 				}
 			}
 
-
-
-
-					char** orginalLab = new  char*[xlab];
-		for (int i = 0; i < xlab; ++i)
-			orginalLab[i] = new char[ylab];
-		{
-
-			for (int i = 0; i < xlab; i++)
+			char** orginalLab = new  char*[xlab];
+			for (int i = 0; i < xlab; ++i)
+				orginalLab[i] = new char[ylab];
 			{
-				for (int j = 0; j < ylab; ++j) //wpisanie wartosci do tablicy
+
+				for (int i = 0; i < xlab; i++)
 				{
-					orginalLab[i][j] = lab[i][j];
+					for (int j = 0; j < ylab; ++j) //wpisanie wartosci do tablicy
+					{
+						orginalLab[i][j] = lab[i][j];
+					}
 				}
 			}
-		}
-
-
-
-
 
 			char** wyniklab = new  char*[xlab];
 			for (int i = 0; i < xlab; ++i)
@@ -151,13 +122,6 @@ int main()
 					}
 				}
 			}
-
-
-
-
-
-			//cout << "xlab== " << xlab << endl;
-			//cout << "ylab== " << ylab << endl;
 
 			Printing(xlab, ylab, lab);
 			botStart(xlab, ylab, lab, startPointx, startPointy, realStartPointx, realStartPointy, lastPositionx, lastPositiony);
@@ -176,9 +140,9 @@ int main()
 				printExit(xlab, ylab, wyniklab);
 				cout << endl << endl << "botowi udalo sie wyjsc z labiryntu!" << endl << endl;
 				cout << "Ilosc wykonanych ruchow: " << ilRuchow << endl;
+				system("Pause");
 				ilRuchow = 0;
 
-				//lab[*startPointx][*startPointy] == 32;
 				*startPointx = *realStartPointx;
 				*startPointy = *realStartPointy;
 
@@ -200,10 +164,8 @@ int main()
 
 				do // I sprawdzenie w druga strone
 				{
-					//cout << "Program wszedl do funkcji odwracajacej kolejknosc sprawdzania" << endl;
 					checkExit = botMove(xlab, ylab, lab, startPointx, startPointy, ptravaNorth, ptravaEast, ptravaSouth, ptravaWest, ptrcrosswayPointx, ptrcrosswayPointy, realStartPointx, realStartPointy, lastPositionx, lastPositiony, wyniklab);
 					Printing(xlab, ylab, lab);
-					//system("pause");
 
 				} while (checkExit == 0);
 				if (checkExit == 1)
@@ -263,7 +225,7 @@ int main()
 					return -1;
 				}
 
-				
+
 
 			}
 			else
@@ -277,7 +239,7 @@ int main()
 
 		plik.close();
 	}
-	else //gdyby z jakiegoÅ› powodu plik zostaÅ‚ nieprawidlowo odczytany, lub nie odczytany w ogÃ³le. (przypomne, ze plik ma sie nazywac labirynt.txt) 
+	else //gdyby z jakiegoœ powodu plik zosta³ nieprawidlowo odczytany, lub nie odczytany w ogóle. (przypomne, ze plik ma sie nazywac labirynt.txt) 
 	{
 		cout << "Error! Blad odczytu pliku!" << endl;
 		system("Pause");
@@ -304,7 +266,6 @@ void Printing(int xlab, int ylab, char** lab)
 		for (int j = 0; j < ylab; ++j)
 			cout << (char)lab[i][j];
 	Sleep(100);
-	//system("Pause");
 	system("cls");
 }
 
@@ -324,7 +285,6 @@ void botStart(int xlab, int ylab, char** lab, int *startPointx, int *startPointy
 	{
 		for (int j = 0; j < ylab; j++)
 		{
-			//cout << "lab[i][j]== " << (int)lab[i][j] << endl;
 			if (lab[i][j] == -80)
 			{
 				startx = i;
@@ -342,15 +302,13 @@ void botStart(int xlab, int ylab, char** lab, int *startPointx, int *startPointy
 	}
 	*startPointx = startx;
 	*startPointy = starty;
-	//cout << "startPointx== " << *startPointx << endl;
-	//cout << "startPointy== " << *startPointy << endl;
 	*realStartPointx = startx;
 	*realStartPointy = starty;
 	*lastPositionx = startx;
 	*lastPositiony = starty;
 }
 
-int botMove(int xlab, int ylab, char** lab, int *startPointx, int *startPointy, bool *ptravaNorth, bool *ptravaEast, bool *ptravaSouth, bool *ptravaWest, int *ptrcrosswayPointx, int *ptrcrosswayPointy, int *realStartPointx, int *realStartPointy, int *lastPositionx,int *lastPositiony,char **wyniklab)
+int botMove(int xlab, int ylab, char** lab, int *startPointx, int *startPointy, bool *ptravaNorth, bool *ptravaEast, bool *ptravaSouth, bool *ptravaWest, int *ptrcrosswayPointx, int *ptrcrosswayPointy, int *realStartPointx, int *realStartPointy, int *lastPositionx, int *lastPositiony, char **wyniklab)
 {
 	*ptravaNorth = false;
 	*ptravaEast = false;
@@ -373,19 +331,6 @@ int botMove(int xlab, int ylab, char** lab, int *startPointx, int *startPointy, 
 		return 1;
 	}
 
-
-	/*
-	
-	moveDirection cheatSheat:
-		1 = North
-		2 = South
-		3 = West
-		4 = East
-	
-	*/
-
-	//cout << "lastPositionx = " << *lastPositionx << " lastPositiony = " << *lastPositiony << endl;
-	//cout << "avaMoves(w tym miejscu ma wynosic zero)= " << avaMoves << endl;
 	if (wDrugaStrone == false)
 	{
 		if (*startPointx - 1 >= 0)
@@ -595,29 +540,14 @@ int botMove(int xlab, int ylab, char** lab, int *startPointx, int *startPointy, 
 	*lastPositionx = *startPointx;
 	*lastPositiony = *startPointy;
 
-	
-	//cout << "wyjacie jest oznaczone numerem: "<<(int)lab[5][4]<<endl;
-	//cout << "startPointx= " << *startPointx << endl <<"startPointy= "<< *startPointy << endl << endl;
-	//cout << endl << "(int)lab[1][1]= " << (int)lab[1][1] << endl;
-	//cout <<"xlab= "<< xlab <<" ylab= "<< ylab << endl;
-	//cout << "testx (z botMove)= " << testx << endl <<"testy (z botMove)= "<< testy << endl;
-	//cout << "avaMoves(East + rest)= " << avaMoves << endl;
-	//cout << "crosswayPointx= " << *ptrcrosswayPointx << " crosswayPointy= " << *ptrcrosswayPointy << endl;
-	//cout << "rozgalezienie= "<<(int)lab[2][3] << endl;
-
-
-	//system("Pause");
 	if (avaMoves == 0) {
-		//cout << "ERROR!!! brak mozliwosci ruchu!!!" << endl;
-		//cout << "Powrot do ostatniego punktu rozwidlenia drog." << endl;
 		cout << "Powrot do punktu startu." << endl;
-		if (*ptrcrosswayPointx == -1 || *ptrcrosswayPointy == -1) 
+		if (*ptrcrosswayPointx == -1 || *ptrcrosswayPointy == -1)
 		{
 			cout << "ERROR!!! po drodze nie znaleziono zadnego punktu rozgalezinia. Powtarzanie labiryntu nie ma sensu!" << endl;
-			//cout << "Konczenie dzialania programu." << endl;
 			return -1;
 		}
-		else 
+		else
 		{
 			ilRuchow++;
 			int startx = -1, starty = -1;
@@ -625,7 +555,6 @@ int botMove(int xlab, int ylab, char** lab, int *startPointx, int *startPointy, 
 			{
 				for (int j = 0; j < ylab; j++)
 				{
-					//cout << "lab[i][j]== " << (int)lab[i][j] << endl;
 					if (lab[i][j] == -2)
 					{
 						lab[i][j] = 32;
@@ -639,114 +568,90 @@ int botMove(int xlab, int ylab, char** lab, int *startPointx, int *startPointy, 
 			lab[*startPointx][*startPointy] = 248;
 			BotBuildWall(xlab, ylab, lab, startPointx, startPointy, ptravaNorth, ptravaEast, ptravaSouth, ptravaWest, *ptrcrosswayPointx, *ptrcrosswayPointy);
 		}
-		
+
 	}
 	else if (avaMoves == 1)
 	{
 		ilRuchow++;
-		BotRealMove(xlab,ylab,lab,moveDirection,startPointx,startPointy, ptrcrosswayPointx, ptrcrosswayPointy,wyniklab);
+		BotRealMove(xlab, ylab, lab, moveDirection, startPointx, startPointy, ptrcrosswayPointx, ptrcrosswayPointy, wyniklab);
 	}
 	else if (avaMoves > 1)
 	{
 		ilRuchow++;
 		*ptrcrosswayPointx = *startPointx;
 		*ptrcrosswayPointy = *startPointy;
-		//cout << "crosswayPointx= " << *ptrcrosswayPointx << " crosswayPointy= " << *ptrcrosswayPointy << endl;
 		BotCrosswayMove(xlab, ylab, lab, startPointx, startPointy, ptravaNorth, ptravaEast, ptravaSouth, ptravaWest, *ptrcrosswayPointx, *ptrcrosswayPointy, wyniklab);
 	}
-	else 
+	else
 	{
 		cout << "ERROR!!! nieznany Blad w funkcji botMove!!! " << endl;
 		return -1;
 	}
-	//testExitx = *startPointx;
-	//testExity = *startPointy;
 
 
 	return 0;
 }
 
-void BotRealMove(int xlab, int ylab, char** lab,int moveDirection, int *startPointx, int *startPointy, int *ptrcrosswayPointx, int *ptrcrosswayPointy,char **wyniklab)
+void BotRealMove(int xlab, int ylab, char** lab, int moveDirection, int *startPointx, int *startPointy, int *ptrcrosswayPointx, int *ptrcrosswayPointy, char **wyniklab)
 {
-	int testx=*startPointx, testy=*startPointy;
+	int testx = *startPointx, testy = *startPointy;
 
 	wyniklab[*startPointx][*startPointy] = 176;
 	lab[*startPointx][*startPointy] = 32;
 
-	//cout << " startPointx= " << *startPointx << " startPointy= " << *startPointy << endl;
-		if (wDrugaStrone == false)
+	if (wDrugaStrone == false)
+	{
+		switch (moveDirection)
 		{
-			switch (moveDirection)
-			{
-			case 1:
-				//cout << "North"<<endl;
-				testx = *startPointx;
-				testx--;
-				break;
-			case 2:
-				//cout << "South" << endl;
-				testx = *startPointx;
-				testx++;
-				break;
-			case 3:
-				//cout << "West" << endl;
-				testy = *startPointy;
-				testy--;
-				break;
-			case 4:
-				//cout << "East" << endl;
-				testy = *startPointy;
-				testy++;
-				break;
-			}
+		case 1:
+			testx = *startPointx;
+			testx--;
+			break;
+		case 2:
+			testx = *startPointx;
+			testx++;
+			break;
+		case 3:
+			testy = *startPointy;
+			testy--;
+			break;
+		case 4:
+			testy = *startPointy;
+			testy++;
+			break;
 		}
-		else 
+	}
+	else
+	{
+		switch (moveDirection)
 		{
-			switch (moveDirection)
-			{
-			case 4:
-				//cout << "North"<<endl;
-				testy = *startPointy;
-				testy++;
-				break;
-			case 3:
-				//cout << "South" << endl;
-				testy = *startPointy;
-				testy--;
-				break;
-			case 2:
-				//cout << "West" << endl;
-				testx = *startPointx;
-				testx++;
-				break;
-			case 1:
-				//cout << "East" << endl;
-				testx = *startPointx;
-				testx--;
-				break;
-			}
+		case 4:
+			testy = *startPointy;
+			testy++;
+			break;
+		case 3:
+			testy = *startPointy;
+			testy--;
+			break;
+		case 2:
+			testx = *startPointx;
+			testx++;
+			break;
+		case 1:
+			testx = *startPointx;
+			testx--;
+			break;
 		}
+	}
 
 
+	lab[testx][testy] = 248;
 
 
-
-		if (*ptrcrosswayPointx == testx && *ptrcrosswayPointy == testy)
-		{
-			//lab[*startPointx][*startPointy] = 219;
-		}
-		//cout << " startPointx= " << *startPointx << " startPointy= " << *startPointy << endl;
-		//cout << " testx= " << testx << " testy= " << testy << endl;
+	*startPointx = testx;
+	*startPointy = testy;
 
 
-			lab[testx][testy] = 248;
-
-			
-
-			*startPointx = testx;
-			*startPointy = testy;
-
-			
 }
 
 void BotCrosswayMove(int xlab, int ylab, char** lab, int *startPointx, int *startPointy, bool *ptravaNorth, bool *ptravaEast, bool *ptravaSouth, bool *ptravaWest, int crosswayPointx, int crosswayPointy, char **wyniklab)
@@ -780,7 +685,7 @@ void BotCrosswayMove(int xlab, int ylab, char** lab, int *startPointx, int *star
 			crosswayLastMove = 3;
 		}
 	}
-	else 
+	else
 	{
 		if (*ptravaEast == true)
 		{
@@ -807,7 +712,7 @@ void BotCrosswayMove(int xlab, int ylab, char** lab, int *startPointx, int *star
 	*startPointx = testx;
 	*startPointy = testy;
 	lab[*startPointx][*startPointy] = 248;
-	
+
 }
 
 void BotBuildWall(int xlab, int ylab, char** lab, int *startPointx, int *startPointy, bool *ptravaNorth, bool *ptravaEast, bool *ptravaSouth, bool *ptravaWest, int crosswayPointx, int crosswayPointy)
@@ -834,7 +739,7 @@ void BotBuildWall(int xlab, int ylab, char** lab, int *startPointx, int *startPo
 			testy++;
 		}
 	}
-	else 
+	else
 	{
 		if (crosswayLastMove == 3)
 		{
@@ -868,88 +773,3 @@ int Lucas(int n)
 	else return Lucas(n - 2) + Lucas(n - 1);
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-	UWAGA!!! UWAGA!!! UWAGA!!! UWAGA!!! UWAGA!!! UWAGA!!! UWAGA!!! UWAGA!!! UWAGA!!! UWAGA!!! 
-
-	Rzeczy, ktore moga sie przydac, ale prawdopodobnie sie nie przydaca.
-*/
-
-//cos z botMove
-
-/*
-
-if (*startPointx-1 <0)
-{
-cout << "ruch w strone polnocy spowodowalby wyjscie poza plansze.";
-ptravaNorth = false;
-}
-
-
-if (*startPointx + 1 > ylab)
-{
-cout << "ruch w strone poludnia spowodowalby wyjscie poza plansze.";
-ptravaSouth = false;
-}
-
-
-if (*startPointy - 1 <0)
-{
-cout << "ruch w strone wschodu spowodowalby wyjscie poza plansze.";
-ptravaEast = false;
-}
-
-
-if (*startPointy + 1 > ylab)
-{
-cout << "ruch w strone zachodu spowodowalby wyjscie poza plansze.";
-ptravaWest = false;
-}
-
-*/
-
-//Lucas
-
-/*
-int n0 = 2, n1 = 1, input = 0;
-long long int n;
-cout << "wprowadz numer iczby lukasa, jaka chcesz otrzymac: ";
-cin >> input;
-cout << "twoja liczba to: " << input << endl;
-if (input == 0)
-{
-cout << "zerowa liczba Lucasa wynosi " << n0 << endl;
-}
-else if (input == 1) {
-cout << "pierwsza liczba lucasa wynosi " << n1 << endl;
-}
-else
-cout << input;
-do
-{
-n = n1 + n0;
-n0 = n1;
-n1 = n;
-input--;
-} while (input > 1);
-cout << " liczba Lucasa wynosi: " << n;
-system("Pause");
-*/
